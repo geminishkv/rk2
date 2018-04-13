45730a3c6bd4887138f56be01f0cabaacee70bfd
Elijah Doman <gshkvbmstu@gmail.com>
2018-04-13 16:03:55 +0300

    Update main.cpp

--- sources/main.cpp
+++ sources/main.cpp
@@ -4,8 +4,8 @@
 
 int main(int argc, char* argv[]) {
 	
-        std::cout << "Вы ввели: " << *(argv + 1) << std::endl;
-	std::string name = std::getenv("rk2");
+        std::cout << "Zhachenie: " << *(argv + 1) << std::endl;
+	std::string name = std::getenv(*(argv + 1));
 	std::cout << name << std::endl;
 
 	return 0;
